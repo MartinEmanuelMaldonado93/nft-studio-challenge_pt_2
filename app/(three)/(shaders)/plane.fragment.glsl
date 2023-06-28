ff78b021e28c531c8c64f1e4f1a68d806831fb7e
Martin Maldonado <martinemanuel93@gmail.com>
2023-06-28 20:46:24 -0300

    decouple shaders

---
+++ app/(three)/(shaders)/plane.fragment.glsl
@@ -0,0 +1,17 @@
+varying vec2 vUv;
+uniform float uMultiplier;
+uniform float uAlpha;
+uniform vec3 uColorA;
+uniform vec3 uColorB;
+uniform float uTime;
+
+void main() {
+	vec2 mulvUv = mod(vUv * uMultiplier, 2.0);// positive 0 - 2
+	float modTime = mod(mulvUv.y + uTime, 1.0);
+	
+	float strength = step(0.5, modTime) * step(0.5, modTime);
+
+	vec3 mixColor = mix(uColorA, uColorB, step(0.5, mulvUv.y) * step(0.5, mulvUv.x));
+	
+	gl_FragColor.rgba = vec4(mixColor, min(strength, uAlpha));
+}
--- app/(three)/PlaneShaderMaterial.tsx
+++ app/(three)/PlaneShaderMaterial.tsx
@@ -2,6 +2,8 @@
 import { Object3DNode } from "@react-three/fiber";
 import { Color, ShaderMaterial, Texture } from "three";
 import { shaderMaterial } from "@react-three/drei";
+import planeVertex from "../(three)/(shaders)/plane.vertex.glsl";
+import planeFragment from "../(three)/(shaders)/plane.fragment.glsl";
 
 type PlaneShaderMaterial = {
 	uAlpha: number;
@@ -21,28 +23,8 @@ export const PlaneShaderMaterial = shaderMaterial(
 		uColorC: new Color(0x000000),
 		uTime: 0,
 	},
-	`
-			varying vec2 vUv;
-			void main() {
-				vUv = uv;//texture
-				gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-			}
-		`,
-	`
-			varying vec2 vUv;
-			uniform float uMultiplier;
-			uniform float uAlpha;
-			uniform vec3 uColorA;
-			uniform vec3 uColorB;
-			uniform float uTime;
-
-			void main() {
-				vec2 mulvUv = mod(vUv * uMultiplier, 2.0);
-				float strength = step(0.5, mod(mulvUv.y + uTime, 1.0)) * step(0.5, mod(mulvUv.x + uTime, 1.0));
-				vec3 mixColor = mix(uColorA, uColorB, step(0.5, mulvUv.y) * step(0.5, mulvUv.x));
-				gl_FragColor.rgba = vec4(mixColor, min(strength, uAlpha));
-			}
-		`
+	planeVertex,
+	planeFragment
 );
 
 declare module "@react-three/fiber" {
